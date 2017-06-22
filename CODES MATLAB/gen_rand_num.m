8b3b94802389210a5ad599e71d16b6d30de16ff0
Chronos 2.0 <chronosrrc@gmail.com>
2017-06-22 13:44:40 +0000

    gen_rand_num.m details added

--- CODES MATLAB/gen_rand_num.m
+++ CODES MATLAB/gen_rand_num.m
@@ -1,3 +1,17 @@
+%#####################################################################################################
+%# Function : generating a sequence of random numbers lying in the range specified by arr            #                                      #   
+%#                                                                                                   #       
+%# Input(s)  : arr(range of numbers), n(number of random numbers to be generated)                    #
+%#                                                                                                   #                
+%# Ouptut(s) : sequence of random numbers lying in the required range                                # 
+%#                                                                                                   #       
+%# Example: Hom_Trans(-90, 10, 5, 90)                                                                #   
+%#                                                                                                   #       
+%#                                                                                                   #               
+%#                                                                                                   #                                       
+%#####################################################################################################
+
+
 function[R] = gen_rand_num(arr,n)
 SET = arr ;
 NSET = length(SET) ;

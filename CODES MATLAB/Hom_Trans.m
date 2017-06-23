3d986826a548659828a7d94565635b052e57420b
Chronos 2.0 <chronosrrc@gmail.com>
2017-06-23 11:23:12 +0000

    Hom_Trans.m edited function details added

--- CODES MATLAB/Hom_Trans.m
+++ CODES MATLAB/Hom_Trans.m
@@ -1,3 +1,17 @@
+%#####################################################################################################
+%# Function : D-H Homogeneous Tranformation Matrix                                                   #   
+%#                                                                                                   #       
+%# Input(s)  : alp (angle twist in deg), a (link length), d (link offset), theta (link angle in deg) #
+%#                                                                                                   #                
+%# Ouptut(s) : T (Homogeneous Transformation matrix)                                                 # 
+%#                                                                                                   #       
+%# Example: Hom_Trans(-90, 10, 5, 90)                                                                #   
+%#                                                                                                   #       
+%#                                                                                                   #               
+%#                                                                                                   #                                       
+%#####################################################################################################
+
+
 function [T] = Hom_Trans(alp,a,d,theta)
 
 T = [cosd(theta), -(cosd(alp)*sind(theta)), sind(alp)*sind(theta), a*cosd(theta);
